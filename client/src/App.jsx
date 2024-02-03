6b26a277bba2532c24eb5f5d3ea35caf297137d4
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-02-03 19:00:08 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className=' bg-slate-800'>Organic Shop </h1>
+    </div>
+  )
+}
+
+export default App
